fcf8a25cafe1e8c20317816e39d9cc8756c6b44a
flautarian <flautarian@gmail.com>
2022-04-10 17:22:41 +0200

    [INIT]Init

--- src/main/java/edu/uoc/epcsd/notification/controllers/NotificationController.java
+++ src/main/java/edu/uoc/epcsd/notification/controllers/NotificationController.java
@@ -1,13 +1,17 @@
 package edu.uoc.epcsd.notification.controllers;
 
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Log4j2
 @RestController
 @RequestMapping("/notifications")
 public class NotificationController {
 
+    @GetMapping(path = "/{id}", produces = "application/json")
+    public @ResponseBody
+    String getNotificationsTest(@PathVariable int id) {
+        return "your id is " + id + "!!!";
+    }
 
 }
