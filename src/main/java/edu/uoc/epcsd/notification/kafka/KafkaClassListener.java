93d8b3269140f43f2be2dc2f109cde3f3cd77815
flautarian <flautarian@gmail.com>
2022-04-12 19:51:29 +0200

    [SEC] Security commit

--- src/main/java/edu/uoc/epcsd/notification/kafka/KafkaClassListener.java
+++ src/main/java/edu/uoc/epcsd/notification/kafka/KafkaClassListener.java
@@ -17,7 +17,6 @@ public class KafkaClassListener {
     @KafkaListener(topics = KafkaConstants.SHOW_TOPIC + KafkaConstants.SEPARATOR + KafkaConstants.COMMAND_ADD, groupId = "group-1")
     void showAdded(Show show) {
         log.trace("showAdded");
-
         notificationService.notifyShowCreation(show);
     }
 }
