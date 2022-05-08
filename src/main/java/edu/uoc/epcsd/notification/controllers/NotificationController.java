3e052ff8c7b8c9a328b6ec206fc5131d3ebec648
flautarian <fgiacconi.p@gmail.com>
2022-05-08 19:08:20 +0200

    [FIX]Little fixes 3

--- src/main/java/edu/uoc/epcsd/notification/controllers/NotificationController.java
+++ src/main/java/edu/uoc/epcsd/notification/controllers/NotificationController.java
@@ -1,15 +1,31 @@
 package edu.uoc.epcsd.notification.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.uoc.epcsd.notification.pojos.Category;
+import edu.uoc.epcsd.notification.pojos.Show;
 import edu.uoc.epcsd.notification.pojos.User;
 import edu.uoc.epcsd.notification.services.UserService;
+import io.swagger.v3.core.util.Json;
 import lombok.extern.log4j.Log4j2;
+import nonapi.io.github.classgraph.json.JSONDeserializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Log4j2
 @RestController
@@ -18,12 +34,44 @@ public class NotificationController {
 
     @Autowired
     private UserService userService;
-    @PostMapping(path = "/enviarNotificacio", produces = "application/json")
+    @PostMapping(path = "/enviarNotificacio")
     @ResponseStatus(HttpStatus.OK)
-    public void enviarNotificacio(@PathVariable("idUser") Long idUser) {
-        // send email / push notification / etc.
-        List<User> userList = userService.getUsersByFavouriteCategory(new Category());
-        log.info("Notifying the user \"" + userList.get(0).getFullName() + "\"");
+    public ResponseEntity enviarNotificacio(@RequestParam Long idShow) throws IOException {
+        Show show = getShowById(idShow);
+        if(Objects.nonNull(show)){
+            processarNotificacions(show);
+            return new ResponseEntity( "Notificacions processades correctament.", HttpStatus.OK);
+        }
+        else return new ResponseEntity( "Error: No s'ha trobat el show especificat", HttpStatus.OK);
+    }
+
+    private void processarNotificacions(Show show) {
+        show.getCategories().stream().forEach(cat ->{
+            List<User> userList = userService.getUsersByFavouriteCategory(cat);
+            userList.stream().forEach(user -> {
+                log.info("Notifying the user \"" + user.getFullName() + "\"");
+            });
+        });
+    }
+
+    private Show getShowById(Long idShow) throws IOException {
+        URL url = new URL("http://localhost:18081/show/?idShow=" + idShow.toString());
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("Content-Type", "application/json");
+        con.connect();
+        if(con.getResponseCode() == 200){
+            BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line+"\n");
+            }
+            br.close();
+            Show show = new ObjectMapper().readValue(sb.toString(), Show.class);
+            return show;
+        }
+        return null;
     }
 
 }
--- src/main/java/edu/uoc/epcsd/notification/pojos/Show.java
+++ src/main/java/edu/uoc/epcsd/notification/pojos/Show.java
@@ -4,13 +4,13 @@ import lombok.*;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 @ToString
 @Getter
 @Setter
 @EqualsAndHashCode
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
 public class Show {
     private Long id;
@@ -24,4 +24,37 @@ public class Show {
     private String status;
     private List<Category> categories;
     private List<Performance> performances;
+
+    public Show() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Show show = (Show) o;
+        return Float.compare(show.price, price) == 0 && duration == show.duration && capacity == show.capacity && Objects.equals(id, show.id) && Objects.equals(name, show.name) && Objects.equals(description, show.description) && Objects.equals(image, show.image) && Objects.equals(onSaleDate, show.onSaleDate) && Objects.equals(status, show.status) && Objects.equals(categories, show.categories) && Objects.equals(performances, show.performances);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description, image, price, duration, capacity, onSaleDate, status, categories, performances);
+    }
+
+    @Override
+    public String toString() {
+        return "Show{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", image='" + image + '\'' +
+                ", price=" + price +
+                ", duration=" + duration +
+                ", capacity=" + capacity +
+                ", onSaleDate=" + onSaleDate +
+                ", status='" + status + '\'' +
+                ", categories=" + categories +
+                ", performances=" + performances +
+                '}';
+    }
 }
